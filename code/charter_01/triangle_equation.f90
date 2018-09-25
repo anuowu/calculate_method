470d3a8d9be71d6f06d8b6f2ced695b3bedf72d7
anuowu <anuo452@outlook.com>
2018-09-25 12:12:38 +0800

    correct the triangle_equation.f90

--- code/charter_01/triangle_equation.f90
+++ code/charter_01/triangle_equation.f90
@@ -12,9 +12,12 @@ program  main
   read(23,*)
   read(23,*) b
   
-  write(*,*) A(2,:)
-  write(*,*) b
+  !A = (2 1 4 3;0 3 2 5;0 0 7 3; 0 0 0 2)
+  !b = (50 49 53 12)
+  !write(*,*) A(2,:)
+  !write(*,*) b
   call up_tri_equ(A, b, n, X)
+  !call down_tri_equ(A, B,n, X)
   write(*,*) X
 
 end program  main
@@ -26,7 +29,7 @@ subroutine up_tri_equ(A, b, n, X)
   real sum
 
   X(n) = b(n)/A(n,n)
-  do i = n-1 , 1
+  do i = n-1 , 1, -1
     sum = 0.0
     do j = i+1, n
       sum = sum + A(i, j)*X(j)
@@ -35,3 +38,21 @@ subroutine up_tri_equ(A, b, n, X)
   end do
 
 end subroutine
+
+subroutine down_tri_equ(A, b, n, X)
+  implicit none
+  integer n, i,j
+  real A(n,n), b(n), X(n)
+  real sum
+
+  X(1) = b(1)/A(1,1)
+  do i =2, n
+    sum = 0.0
+    do j = 1,i-1
+      sum = sum+A(i,j)*X(j)
+    end do
+    X(i) = (b(i)-sum)/A(i,i)
+  end do
+
+end subroutine
+
